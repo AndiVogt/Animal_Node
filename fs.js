3754d33867aee50b1d718acfaca201853f6e4a15
Andi Vogt <andivogt-tt@gmx.de>
2022-11-21 23:23:55 +0100

    added cors and express, handels POST-Request

--- fs.js
+++ fs.js
@@ -1,11 +1,23 @@
 // TODO: Try soxket -> https://docs.python.org/3/howto/sockets.html
 const http = require('http');
 const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
 const port = 3000;
-
+const app = express();
 const server = http.createServer(function(req, res) {
+  
   console.log(req.headers);
+
+  res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+  res.write("Formular übermittelt");
+  res.end('End of Message to Browser!');
 });
+
+app.use(cors({
+  origin: "*",
+}))
+
 
 server.listen(port, function() {
   console.log("listening on Port 3000");
