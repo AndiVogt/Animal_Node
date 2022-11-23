26871d1c1a8d9ec241c053d6f8d790c61ad6158c
Andi Vogt <andivogt-tt@gmx.de>
2022-11-24 00:10:51 +0100

    update

--- fs.js
+++ fs.js
@@ -3,6 +3,7 @@ const http = require('http');
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
+const { response } = require('express');
 const app = express();
 const port = 8080;
 // const server = http.createServer(function(req, res) {
@@ -13,19 +14,26 @@ const port = 8080;
 //   res.write("Formular übermittelt");
 //   res.end('End of Message to Browser!');
 // });
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
-  response = res.send({
-  'Get' :  'Success'
-  
-  })  
-  console.log("Get Request Successful")
+
+
+
+app.get('/get-test', (req, res) => {
+  fs.readFile("./data.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+    res.send(JSON.parse(jsonString));
+    });
 })
 
 app.post('/post-test', (req, res) => {
   console.log(req.method);
   console.log(req.url);
+  console.log(req.content);
+  console.log(req.text);
   console.log(req.hostname);
   console.log(req.body);
   res.send('Post arrived');
